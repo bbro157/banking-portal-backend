4b6b8fecf741ce56a6f8e1852af3fabf446588e6
Bruce Brown <bbro157@lsu.edu>
2026-03-16 23:05:41 -0500

    Add database connection and schema

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
-# This is a sample Python script.
+from db import get_connection
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def test_db():
+    conn = get_connection()
+    cur = conn.cursor()
+
+    cur.execute("""
+        SELECT u.username, a.account_type, a.balance, a.account_number
+        FROM users u
+        JOIN accounts a ON u.id = a.user_id;
+    """)
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
+
+    cur.close()
+    conn.close()
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+if __name__ == "__main__":
+    test_db()
